05c1db1a531424e92224a8aec9066d8bf8ab0059
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-07-07 21:36:39 -0700

    01.extra-3

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,18 +3,21 @@
 
 import * as React from 'react'
 
-// Our count reducer was updated to accept the whole state object and the updated version of that state object. 
-// We merged these two things to get our new state values.
-const countReducer = (state, action) => ({...state, ...action})
+// all that we needed to change here to support both the function and the action object is we checked the type of the 
+// action, and if it's a function, then we called that action with the current state. 
+// Otherwise, we assume the action was the object that we wanted to spread, and we spread that value in either case.
+const countReducer = (state, action) => ({
+  ...state, 
+  ...(typeof action === 'function' ? action(state) : action),
+})
 
-// we changed our state from a number to an object that has a number as a property. We plucked that property off.
-// We updated our increment function. We called setState with an object with a property for the value that we want to have updated. 
 function Counter({initialCount = 0, step = 1}) {
   const [state, setState] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
-  const increment = () => setState({count: count + step})
+  const increment = () =>
+    setState(currentState => ({count: currentState.count + step}))
   return <button onClick={increment}>{count}</button>
 }
 
