ae48d654100c8078a53b4ff3780de67989e379c7
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2021-07-07 21:44:25 -0700

    01.extra-04

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,23 +3,37 @@
 
 import * as React from 'react'
 
-// all that we needed to change here to support both the function and the action object is we checked the type of the 
-// action, and if it's a function, then we called that action with the current state. 
-// Otherwise, we assume the action was the object that we wanted to spread, and we spread that value in either case.
-const countReducer = (state, action) => ({
-  ...state, 
-  ...(typeof action === 'function' ? action(state) : action),
-})
 
+// what we did here was we had this setState type of simulation here with our count reducer, 
+// const countReducer = (state, action) => ({
+//   ...state, 
+//   ...(typeof action === 'function' ? action(state) : action),
+// })
+
+// but as your reducer gets more complex, having this format for your reducer can make it a little bit easier to maintain.
+//  This convention isn't a requirement of using React useReducer, but it's helpful for some cases.
+function countReducer(state, action) {
+  switch (action.type) {
+    case 'INCREMENT': {
+      return {count: state.count + action.step}
+    }
+    default: {
+      throw new Error(`Unsupported action type: ${action.type}`)
+    }
+  }
+}
+// but we wanted to have a more Redux style reducer, not just because the convention is a great one, 
+// and should always be applied.
 function Counter({initialCount = 0, step = 1}) {
-  const [state, setState] = React.useReducer(countReducer, {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
-  const increment = () =>
-    setState(currentState => ({count: currentState.count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
+
+
 
 function App() {
   return <Counter />
